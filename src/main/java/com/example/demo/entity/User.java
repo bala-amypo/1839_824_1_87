de01ec4a6ca54677750a55518bf80fc13f4afee1
bala-amypo <bala@amypo.in>
2025-12-17 09:21:57 +0000

    User commit

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -1,64 +1,6 @@
-package com.example.demo.entity;
-
 import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Column;
-import jakarta.persistence.PrePersist;
-import java.time.LocalDateTime;
-
 @Entity
-public class User {
-
+public class User{
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
 
-    @Column(unique = true, nullable = false)
-    private String categoryName;
-
-    private String description;
-    private LocalDateTime createdAt;
-
-    public User() {
-    }
-
-   
-    public User(String categoryName, String description) {
-        this.categoryName = categoryName;
-        this.description = description;
-    }
-
-    public void onCreate() {
-        this.createdAt = LocalDateTime.now();
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getCategoryName() {
-        return categoryName;
-    }
-
-    public void setCategoryName(String categoryName) {
-        this.categoryName = categoryName;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
 }
