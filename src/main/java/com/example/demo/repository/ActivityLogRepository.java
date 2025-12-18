1c3f1416a9dc5ee9f7d13b45c64a7741ca782397
bala-amypo <bala@amypo.in>
2025-12-18 08:59:33 +0000

    User commit

--- src/main/java/com/example/demo/repository/ActivityLogRepository.java
+++ src/main/java/com/example/demo/repository/ActivityLogRepository.java
@@ -1,8 +1,8 @@
 package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.entity.User;
+import com.example.demo.entity.ActivityLog;
 
-public interface ActivityLogRepository extends JpaRepository<User, Long> {
+public interface ActivityLogRepository extends JpaRepository<ActivityLog, Long> {
 
 }
--- src/main/java/com/example/demo/repository/ActivityTypeRepository.java
+++ src/main/java/com/example/demo/repository/ActivityTypeRepository.java
@@ -3,6 +3,6 @@ package com.example.demo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.entity.ActivityType;
 
-public interface ActivityTypeRepository extends JpaRepository<ActivtyType, Long> {
+public interface ActivityTypeRepository extends JpaRepository<ActivityType, Long> {
 
 }
