3eb8a86e8550c818841379d6723a5f7b15b30337
bala-amypo <bala@amypo.in>
2025-12-18 15:46:53 +0000

    User commit

--- src/main/java/com/example/demo/entity/ActivityCategory.java
+++ src/main/java/com/example/demo/entity/ActivityCategory.java
@@ -1,47 +1,53 @@
 package com.example.demo.entity;
 
-import jakarta.persistence.*;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.PrePersist;
+import java.time.LocalDateTime;
 
 @Entity
-@Table(
-    name = "activity_categories",
-    uniqueConstraints = {
-        @UniqueConstraint(columnNames = "name")
-    }
-)
-public class ActivityCategory {
-
+public class ActivityCategory
+{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String categoryName;
+    private String description;
+    private LocalDateTime createdAt;
 
-    @Column(nullable = false, unique = true)
-    private String name;
+public long getId(){
+    return id; 
+}
+public void setId(Long id){
+    this.id=id;
+}
+public String getCategoryName(){
+    return categoryName;
+}
+public void setCategoryName(String categoryName){
+    this.categoryName=categoryName;
+}
+public String getDescription(){
+    return description;
+}
+public void setDescription(String description){
+    this.description=description;
+}
+public LocalDateTime getCreatedAt(){
+    return createdAt;
 
-    // =====================
-    // Constructors
-    // =====================
-
-    public ActivityCategory() {
-    }
-
-    public ActivityCategory(String name) {
-        this.name = name;
-    }
-
-    // =====================
-    // Getters & Setters
-    // =====================
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
+}
+public void setCreatedAt(LocalDateTime createdAt){
+this.createdAt=createdAt;
+}
+public ActivityCategory(Long id,String categoryName,String description,LocalDateTime createdAt){
+    this.id=id;
+    this.categoryName=categoryName;
+    this.description=description;
+    this.createdAt=createdAt;
+}
+public ActivityCategory(){}
 }
